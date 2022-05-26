98fa3bd5b35d55ed84c1b2b68b622c28ddb5a01c
lj259 <jacob.j.lee259@gmail.com>
2022-05-26 13:18:25 +1200

    Added the feature of stamina bar increasing after a set time
    Stamina bar increases after a set time.

--- 2D Game y13/Assets/Player.cs
+++ 2D Game y13/Assets/Player.cs
@@ -6,10 +6,14 @@ public class Player : MonoBehaviour
 {
     public int maxStamina = 10;
     public int currentStamina;
-
+    float time;
+    float timeDelay;
     public Staminabar StaminaBar;
+
     void Start()
     {
+        time = 0f;
+        timeDelay = 8f;
         currentStamina = maxStamina;
         StaminaBar.SetMaxStamina(maxStamina);
     }
@@ -17,14 +21,34 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
             TakeStam(1);
         }
+
+        time = time + 1f * Time.deltaTime;
+        if (time >= timeDelay)
+        {
+            time = 0f;
+            if(currentStamina < maxStamina)
+            {
+                UpStam(1);
+            }
+        }
+        
     }
     void TakeStam(int stam)
     {
         currentStamina -= stam;
+
+        StaminaBar.SetStamina(currentStamina);
+    }
+
+    void UpStam (int stam2)
+    {
+        
+        currentStamina += stam2;
 
         StaminaBar.SetStamina(currentStamina);
     }
